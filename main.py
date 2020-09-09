792e6856ac9cfc0c9d9e847ebb76d94f819a879f
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-09 10:16:54 +0300

    refactoring: made tray icon function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import PySimpleGUIQt as sg
 
-if __name__ == '__main__':
+
+def show_tray_icon():
     menu_def = ['BLANK', ['&Open', '---', '&Save', ['1', '2', ['a', 'b']], '&Properties', 'E&xit']]
-
     tray = sg.SystemTray(menu=menu_def, filename=r'icon.png')
-
     while True:  # The event loop
         menu_item = tray.read()
         print(menu_item)
@@ -12,3 +11,7 @@ if __name__ == '__main__':
             break
         elif menu_item == 'Open':
             sg.popup('Menu item chosen', menu_item)
+
+
+if __name__ == '__main__':
+    show_tray_icon()
