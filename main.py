693f8e3bec6b05827da4352568daba359e53bf49
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-09 11:20:02 +0300

    add: logging

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
+import logging
 import time
 import threading
+from tkinter import Tk
 
 import PySimpleGUIQt as sGUI
 import alarm as a
 
 
 def show_tray_icon():
-    menu_def = ['BLANK', ['&Open', '---', '&Save', ['1', '2', ['a', 'b']], '&Properties', 'E&xit']]
+    menu_def = ['', ['&Настройки', '&Выход']]
     tray = sGUI.SystemTray(menu=menu_def, filename=r'icon.png')
     while True:  # The event loop
         menu_item = tray.read()
         print(menu_item)
-        if menu_item == 'Exit':
+        if menu_item == 'Выход':
             break
-        elif menu_item == 'Open':
-            sGUI.popup('Menu item chosen', menu_item)
+        elif menu_item == 'Настройки':
+            sGUI.popup('Menu item chosen')
 
 
 def main_thread(alarm: a.Alarm):
+    logging.info("Running daemon")
     ring_time = False
     while True:
         if alarm.is_ring_time() and not ring_time:
             ring_time = True
-            pass
         else:
             ring_time = False
         time.sleep(5)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
     alarm_entity = a.Alarm()
     main_loop = threading.Thread(target=main_thread, args=(alarm_entity,), daemon=True)
+    main_loop.start()
+
     show_tray_icon()
