f0c86d7c598a967dfd926a45d5566d52c742db9a
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-09 10:26:37 +0300

    add: test function

--- alarm.py
+++ alarm.py
@@ -10,3 +10,13 @@ class Alarm(object):
 
     def is_ring_time(self):
         return time.strftime('%H:%M %a') in self.alarms
+
+
+if __name__ == '__main__':
+    alarm = Alarm(['10:26 Wed'])
+    while True:
+        if alarm.is_ring_time():
+            print("Ala")
+        else:
+            print("not yet")
+        time.sleep(2)
