0ff816834ed83f637ba775ebd2d818d1a4dc4320
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-10 05:11:26 +0300

    add: weekDays select option (GUI only)
    Took 1 hour 20 minutes

--- settings.py
+++ settings.py
@@ -1,14 +1,24 @@
 from tkinter import *
 
+week_days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
+
+
+def weeks_selector_create(container):
+    f_left = LabelFrame(container, text="Дни недели")
+    f_left.pack()
+    check_box = []
+    week_sel = []
+    for num, day in enumerate(week_days):
+        week_sel.append(BooleanVar())
+        check_box.append(
+            Checkbutton(f_left, text=day, variable=week_sel[num], onvalue=1, offvalue=0, indicatoron=0))
+        check_box[num].pack(side=LEFT)
+
 
 def run():
     root = Tk()
     root.title("Настройки будильника")
-    value = StringVar(root)
-    value.set("Monday")  # default value
-    options = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", " Sunday"]
-    week_selector = OptionMenu(root, value, *options)
-    week_selector.pack()
+    weeks_selector_create(root)
     root.mainloop()
 
 
