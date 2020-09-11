29bf39cafbe047ae2fb15ca9aec0f80a2f8a10d1
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-11 20:21:56 +0300

    edit: added validation
    Took 19 minutes

--- settings.py
+++ settings.py
@@ -3,6 +3,10 @@ from tkinter import *
 week_days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
 
 
+def is_valid(input_string: str):
+    return input_string.isdigit() and len(input_string) < 3
+
+
 def create_week_days_selector(container):
     f_days = LabelFrame(container, text="Дни недели")
     f_days.pack(ipadx=5, ipady=5, padx=5, pady=5)
@@ -18,11 +22,17 @@ def create_week_days_selector(container):
 def create_time_selector(container):
     f_time = LabelFrame(container, text="Время")
     f_time.pack()
-    hour_entry = Spinbox(f_time, width=4, from_=0, to=23, wrap=True, font='Arial 20')
-    minute_entry = Spinbox(f_time, width=4, from_=0, to=59, wrap=True, font='Arial 20')
+
+    time_font = 'Arial 30'
+    hour_entry = Spinbox(f_time, width=4, from_=0, to=23, wrap=True, font=time_font)
+    minute_entry = Spinbox(f_time, width=4, from_=0, to=59, wrap=True, font=time_font)
     hour_entry.pack(side=LEFT)
-    Label(f_time, text=":", font='Arial 20').pack(side=LEFT)
+    Label(f_time, text=':', font=time_font).pack(side=LEFT)
     minute_entry.pack(side=LEFT)
+
+    reg = container.register(is_valid)
+    hour_entry.config(validate='key', validatecommand=(reg, '%P'))
+    minute_entry.config(validate='key', validatecommand=(reg, '%P'))
 
 
 def run():
