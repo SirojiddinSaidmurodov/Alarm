60de27bd5baad66ad567adc450f20751c0b5f72f
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-09-11 23:25:07 +0300

    edit: Alarm class redefined
    Took 1 hour 26 minutes

--- alarmcontainer.py
+++ alarmcontainer.py
@@ -14,36 +14,19 @@ class AlarmContainer(object):
         return Alarm() in self.alarms
 
 
-def format_days(week_day):
-    result_list = []
-    for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", " Sunday"]:
-        if day in week_day:
-            result_list.append(day)
-        else:
-            result_list.append("")
-    return result_list
-
-
 class Alarm(object):
-    def __init__(self, time=t.strftime('%H:%M'), week_days=None):
-        if week_days is None:
-            self.weekdays = t.strftime('%a')
-        else:
-            self.weekdays = format_days(week_days)
+    def __init__(self, time=t.strftime('%H:%M'), weekdays=2 ** int(t.strftime('%w'))):
+        self.weekdays = weekdays
         self.time = time
+
+    def __str__(self):
+        result = self.time
+        result += "  "
+        result += str(bin(self.weekdays))
+        return result
 
     def __eq__(self, other):
         if self.time == other.time:
-            if self.weekdays in other.weekdays:
+            if self.weekdays & other.weekdays:
                 return True
         return False
-
-
-if __name__ == '__main__':
-    alarm = AlarmContainer([Alarm('03:41', 'Thursday')])
-    while True:
-        if alarm.is_ring_time():
-            print("Ala")
-        else:
-            print("not yet")
-        t.sleep(2)
